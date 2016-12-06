ef1784654535da280198812b036ab3edc924c641
Maddie Cousens <mscousens@gmail.com>
2016-12-06 06:13:23 +0000

    Added test case to reverse_string

--- Recursion-reverse_string.py
+++ Recursion-reverse_string.py
@@ -2,6 +2,22 @@
 #new string that is the reverse of the old string.
 
 def reverse_string(word):
+    """
+    Returns string reversed
+
+    >>> reverse_string('maddie')
+    'eiddam'
+
+
+    """
     if len(word) <= 1:
         return word
     return word[-1] + reverse_string(word[:-1])
+
+if __name__ == '__main__':
+
+    import doctest
+
+    result = doctest.testmod()
+    if not result.failed:
+        print "All tests passed!"
